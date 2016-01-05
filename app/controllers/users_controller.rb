06929291182336f2aec7ed54a9549b3c10ba6b46
Julianne Gendrano <jujukoo@gmail.com>
2016-01-05 16:27:49 -0500

    Fix hound barking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
@@ -14,11 +13,10 @@ class UsersController < ApplicationController
     end
   end
 
-private
+  private
+  
   def user_params
-    params
-      .require(:user)
-      .permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email,
+                                 :password, :password_confirmation)
   end
-
 end
