fc0c9f2df3172e442da14a299186e1dcfef8a29e
Julianne Gendrano <jujukoo@gmail.com>
2016-01-05 16:38:08 -0500

    Fixed futher Hound issues.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,12 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email,
-                                 :password, :password_confirmation)
+    params.require(:user).permit(:first_name,
+                                 :last_name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
   end
 end
