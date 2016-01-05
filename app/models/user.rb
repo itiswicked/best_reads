5559cbd9829144c326d70cc60cfa9827c5e7bd7f
Julianne Gendrano <jujukoo@gmail.com>
2016-01-05 16:42:45 -0500

    Hound issues further resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
