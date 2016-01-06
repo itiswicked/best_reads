3bdbbffaefb0b2c09c0acb24eeab236550ae82f9
JosephWinfield <krazyjoe18@gmail.com>
2016-01-06 16:06:24 -0500

    delete superfluous flash notifications

--- db/schema.rb
+++ db/schema.rb
@@ -58,5 +58,4 @@ ActiveRecord::Schema.define(version: 20160105184142) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-
 end
