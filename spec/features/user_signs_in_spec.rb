e07306996198a61669ec09e5e463df6a1cae2b49
Olivia <oereeve@gmail.com>
2016-01-06 11:01:03 -0500

    user signs in

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+feature 'user signs in', %Q{
+  As an unauthenticated user
+  I want to sign in
+  So that I can post items and review them
+} do
+  # Acceptance Criteria:
+  # [] If I specify a valid, previously registered email and password, I am authenticated and I gain access to the system
+  # [] If I specify an invalid email and password, I remain unauthenticated
+  # [] If I am already signed in, I can't sign in again
+
+  scenario 'an existing user specifies a valid email and password' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    expect(page).to have_content('Welcome Back!')
+    expect(page).to have_content('Sign Out')
+  end
+
+  scenario 'a non-existant user email and password is supplied' do
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'nobody@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Sign In'
+
+    expect(page).to_not have_content('Welcome Back!')
+    expect(page).to_not have_content('Sign Out')
+    expect(page).to have_content('Invalid email or password')
+  end
+
+  scenario 'an existing email with the wrong password is denied access' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'incorrectPassword'
+    click_button 'Sign In'
+
+    expect(page).to have_content('Invalid email or password')
+    expect(page).to_not have_content('Sign Out')
+  end
+
+  scenario 'an already authenticated user cannot re-sign in' do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    expect(page).to have_content('Sign Out')
+    expect(page).to_not have_content('Sign In')
+
+    visit new_user_session_path
+    expect(page).to have_content('You are already signed in.')
+  end
+
+end
