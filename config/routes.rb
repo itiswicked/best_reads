7fe4de6ce53847ac3f42e2d3bdeda8251ab28a3d
JosephWinfield <krazyjoe18@gmail.com>
2016-01-07 10:42:58 -0500

    hopefully fixes merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
-
   root 'landings#index'
+
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   resources :books
 
   resources :authors, except: [:destroy] do
     resources :books
   end
-
-  devise_for :users
 end
