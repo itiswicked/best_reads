5201d01e59bb996438ef3fd7b4f4dcd7e1b4b7c4
Olivia <oereeve@gmail.com>
2016-01-08 13:16:24 -0500

    user reviews changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,6 @@ class Review < ActiveRecord::Base
   belongs_to :book
   belongs_to :user
 
-  validates :title, presence: true
-  validates :body, presence: true, length: { maximum: 500 }
+  validates :body, length: { maximum: 500 }
   validates :rating, presence: true
-  # validates :book_id, uniqueness: { scope: :user_id, message: "You've reviewed this book!" }
 end
