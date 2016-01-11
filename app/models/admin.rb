91cfc40f249097576b56743d9cf018ec1c7b9364
Julianne Gendrano <jujukoo@gmail.com>
2016-01-11 10:13:15 -0500

    Friday changes

---
+++ app/models/admin.rb
@@ -0,0 +1,11 @@
+class Admin < ActiveRecord::Base
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
---
+++ spec/features/administrator_role_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+# As an administrator
+# I want to be able to delete any review
+# if the content is deemed inappropriate
+#
+# Acceptance Criteria:
+#
+# [] Allows an Administrator to view a list of Users
+# [] Allows Administrator to delete a User
+# [] Allows Administrator to delete a Book
+# [] Allows Administrator to delete a Review
