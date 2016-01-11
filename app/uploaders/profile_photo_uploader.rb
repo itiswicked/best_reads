61181869baa34d4e8f27e7bbe2904568644a906f
JosephWinfield <krazyjoe18@gmail.com>
2016-01-11 17:00:59 -0500

    fix hound violations

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,9 +1,4 @@
-# encoding: utf-8
-
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
-
-  # process resize_to_fit: [800, 800]
-
   if Rails.env.test?
     storage :file
   else
@@ -13,5 +8,4 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,6 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
     }
-    config.fog_directory  = ENV["S3_BUCKET"]
+    config.fog_directory = ENV["S3_BUCKET"]
   end
 end
