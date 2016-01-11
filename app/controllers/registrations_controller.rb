2fae70b349858b0ec8a65dea873d82982ee21fae
JosephWinfield <krazyjoe18@gmail.com>
2016-01-11 17:07:05 -0500

    fix line spacing violations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,6 +14,6 @@ class RegistrationsController < Devise::RegistrationsController
       :password,
       :password_confirmation,
       :current_password
-      )
+                                )
   end
 end
