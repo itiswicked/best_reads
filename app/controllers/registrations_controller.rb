a6c48a72686b3fcae53844c166263b69ad957d2e
JosephWinfield <krazyjoe18@gmail.com>
2016-01-11 17:09:33 -0500

    fix line spacing violations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,14 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email,
-                                 :password, :password_confirmation, :profile_photo)
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :profile_photo
+                                )
   end
 
   def account_update_params
