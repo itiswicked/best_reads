f7e74f2dcf764674592a5a7ea66d156daecfcb70
Julianne Gendrano <jujukoo@gmail.com>
2016-01-13 11:45:02 -0500

    initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authorize_user, except: [:index, :show]
+
   def index
     @books = Book.order(:title).page params[:page]
   end
@@ -54,4 +56,10 @@ class BooksController < ApplicationController
   def genres_collection
     Genre.all.map { |genre| [genre.genre_name, genre.id] }
   end
+  
+  def authorize_user
+  if !user_signed_in? || !current_user.admin?
+    raise ActionController::RoutingError.new("Not Found")
+  end
+end
 end
