edaa60de85d24aec5dbdcd6bc6d2271f42f1150b
Nathan <nathanrbourke@gmail.com>
2016-01-14 13:45:41 -0500

    push for others

--- spec/features/user_updates_review_spec.rb
+++ spec/features/user_updates_review_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+extend UpdateReviewsHelpers
 
 feature 'user updates review' do
   # As a user
@@ -21,31 +22,16 @@ feature 'user updates review' do
 
   before(:each) { login_as(users_review.user, scope: :user) }
 
-  def expect_fields_autopopulated
-    expect(find_field('Title').value).to eq users_review.title
-    expect(find_field('Review').value).to eq users_review.body
-  end
-
-  def fill_in_form
-    fill_in 'Title', with: 'I hated it so much'
-    fill_in 'Review',
-            with: 'So much that flames were coming out the sides of my face'
-    select '4', from: 'Rating'
-    click_button 'Update Review'
-  end
-
   scenario 'accesses from book\'s show page' do
     visit book_path(users_review.book)
 
     expect(page).to have_content users_review.title
     expect(page).to have_content other_review.title
 
-    users_review_elem = page.find('li', text: users_review.title)
-
     click_button 'Edit'
     expect_fields_autopopulated
-    fill_in_form
-save_and_open_page
+    fill_in_update_form
+
     expect(page).to have_content users_review.book.title
   end
 
@@ -54,9 +40,8 @@ save_and_open_page
 
     click_button 'Edit'
     expect_fields_autopopulated
-    fill_in_form
+    fill_in_update_form
 
-    # Redirect to user's profile page
     expect(page).to have_content "Hello #{users_review.user.first_name}!"
   end
 
---
+++ spec/support/update_reviews_helpers.rb
@@ -0,0 +1,18 @@
+module UpdateReviewsHelpers
+  def expect_fields_autopopulated
+    review = find_field('Title').value
+    expect(review).to eq users_review.title
+    review = find_field('Review').value
+    expect(review).to eq users_review.body
+  end
+
+  def fill_in_update_reviews_form
+    fill_in 'Title', with: 'I hated it so much'
+    fill_in(
+      'Review',
+      with: 'So much that flames were coming out the sides of my face'
+    )
+    select '4', from: 'Rating'
+    click_button 'Update Review'
+  end
+end
