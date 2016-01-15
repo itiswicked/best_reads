e6ff4e2b25a8ccb0b67632b1cb134d8fe91ba297
Julianne Gendrano <jujukoo@gmail.com>
2016-01-15 07:30:29 -0500

    minor changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -87,7 +87,7 @@ class BooksController < ApplicationController
   end
 
   def authorize_user
-    if !user_signed_in? || !current_user.admin?
+    if !user_signed_in? && !current_user.admin?
       raise ActionController::RoutingError.new('Not Found')
     end
   end
